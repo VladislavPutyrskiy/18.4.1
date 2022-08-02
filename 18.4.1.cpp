bbae3716e998c76dfdb9da521d5cf3015ef96854
Vladislav Putyrskiy <V.V.Putyrskiy@gmail.com>
2022-08-02 16:25:37 +0300

    Добавьте файлы проекта.

---
+++ 18.4.1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+void evendigits(long long& n, int* ans)
+{
+    while (n)
+    {
+        ++* ans;
+        n /= 10;
+        evendigits(n, ans);
+    }
+
+}
+
+int main()
+{
+    int ans = 0;
+    long long number = 9223372036854775806;
+    evendigits(number, &ans);
+    std::cout << ans;
+}
